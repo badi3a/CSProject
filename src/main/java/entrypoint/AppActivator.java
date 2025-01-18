4362fdac3b2b809b819a79ac22bce3a9a35d3a17
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2025-01-18 15:21:13 +0100

    First JaxRs Project

---
+++ src/main/java/entrypoint/AppActivator.java
@@ -0,0 +1,8 @@
+package entrypoint;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+//the first class to be run: main endPoint
+@ApplicationPath("api/")
+public class AppActivator extends Application {
+}
